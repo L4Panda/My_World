201e94a8ec2675dea3ad2957577a05545bb08e92
Destiny Brown <dbbrown14@students.desu.edu>
2018-03-14 12:32:01 -0400

    City World
    Creation of the City World for the game

---
+++ Assets/Scripts/buildCity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildCity : MonoBehaviour {
+
+    public GameObject[] buildings;
+    public int mapWidth = 20;
+    public int mapHeight = 20;
+    int buildingFootprint = 3;
+
+    // Use this for initialization
+	void Start () {
+		for(int h = 0; h < mapHeight; h++)
+        {
+            for(int w = 0; w < mapWidth; w++)
+            {
+                int result = (int)(Mathf.PerlinNoise(w/10.0f, h/10.0f) * 10);
+                Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
+                if (result < 2)
+                    Instantiate(buildings[0], pos, Quaternion.identity);
+                else if (result < 4)
+                    Instantiate(buildings[1], pos, Quaternion.identity);
+                else if (result < 6)
+                    Instantiate(buildings[2], pos, Quaternion.identity);
+                else if (result < 8)
+                    Instantiate(buildings[3], pos, Quaternion.identity);
+                else if (result < 10)
+                    Instantiate(buildings[4], pos, Quaternion.identity);
+            }
+        }
+	}
+	
+}
